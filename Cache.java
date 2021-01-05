b6e4080382fc23e06278eebb69f79a87bd34c66e
robi <robi_jeanne_banogon@dlsu.edu.ph>
2021-01-05 11:20:41 +0800

    Update Cache.java

--- Cache.java
+++ Cache.java
@@ -1,7 +1,41 @@
-public class Cache
-{
-    
+public class Cache {
+
     public Cache ()
 
-    
+    public int getCacheSize() {
+        return this.cachesize;
+    }
+
+    public int getBlockSize() {
+        return this.blocksize;
+    }
+
+    public int getSetSize() {
+        return this.setsize;
+    }
+
+    public int getCacheHit() {
+        return this.cachehit;
+    }
+
+    public int getCacheMiss() {
+        return this.cachemiss;
+    }
+
+    public int getMissPenalty() {
+        return this.misspenalty;
+    }
+
+    public Cache getCache() {
+        return this.Cache;
+    }
+
+    public void saveSnapshot() {
+
+    }
+
+    public void saveFile() {
+
+    }
+
 }
