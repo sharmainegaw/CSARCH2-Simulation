44b740a9efc9e98a9c6fe054140bab5c90be0321
sharmainegaw <sharmaine_gaw@dlsu.edu.ph>
2021-01-07 13:11:42 +0800

    added more test cases

--- Driver.java
+++ Driver.java
@@ -1,20 +1,13 @@
 public class Driver {
     public static void main(String[] args)
     {
-       /* 
-       cache size: 32 bytes, 8 blocks
-        block size: 4 bytes
-        data: 200, 204, 208, 20C, 2F4, 2F0, 200, 204, 218, 21C, 24C, 2F4
-        set size: none
-        */
-       String[] data = {"200","204","208","20C","2F4","2F0","200","204","218","21C","24C","2F4"};
-       Cache item4_C = new Cache(32,8,0,false,1);   // cache size, blocks, set size, isLT?, access time
-       MainMemory  item4_MM = new MainMemory(1);     // access time
-       item4_MM.loadData(data);
+        String[] data = {"200","204","208","20C","2F4","2F0","200","204","218","21C","24C","2F4"};
+        Cache item4_C = new Cache(8,8,2,false,1);   // cache size, blocks, set size, isLT?, access time
+        MainMemory  item4_MM = new MainMemory(1);     // access time
+        item4_MM.loadData(data);
 
-       item4_C.simulate(data);
-
-       // item4_MM.transferData(item4_C);
+        String[] dataInBlocks = {"21", "27", "25", "20", "29", "27", "21", "25", "30", "30"};
+        item4_C.simulate(dataInBlocks, Boolean.TRUE, Boolean.FALSE);
 
     }
 }
